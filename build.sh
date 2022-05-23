5f16517a1252878aae00ee0db1b56dc4bbd92736
ks <ks>
2022-05-23 11:21:11 -0700

    output binaries go to output/ and git ignore it

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ g++ -c -I. $no_warnings src/crx.c
 if [ -f crx.o ]; then
 	ls -l crx.o
 
-	g++ -I. $no_warnings crx.o src/usage.c -o usage
-	g++ -I. $no_warnings crx.o src/unittest.c -o unittest && ./unittest
+	g++ -I. $no_warnings crx.o src/usage.c -o output/usage
+	g++ -I. $no_warnings crx.o src/unittest.c -o output/unittest && output/unittest
 fi
