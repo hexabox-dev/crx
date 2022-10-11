13bb8d9eb3e943b67f79c3178d195aeb574225f5
ks <ks>
2022-10-11 12:12:24 -0700

    g++ no write strings flag for linux

--- build.sh
+++ build.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-no_warnings="-Wno-deprecated -Wno-c++11-compat-deprecated-writable-strings"
+if [ "`uname -s`" == "Darwin" ]; then
+	no_warnings="-Wno-deprecated -Wno-c++11-compat-deprecated-writable-strings"
+else
+	no_warnings="-Wno-write-strings"
+fi
 
 g++ -c -I. $no_warnings src/crx.c
 
 if [ -f crx.o ]; then
 	ls -l crx.o
 
+	if [ ! -d output ]; then mkdir output; fi
 	g++ -I. $no_warnings crx.o src/usage.c -o output/usage
 	g++ -I. $no_warnings crx.o src/unittest.c -o output/unittest && output/unittest
 fi
